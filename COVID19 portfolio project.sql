b462e2ac30e6f5b9d0e4b67d7a0fd08243e22dd8
Moahmed01 <113978006+Moahmed01@users.noreply.github.com>
2023-02-06 12:29:57 +1000

    Update COVID19 portfolio project.sql

--- COVID19 portfolio project.sql
+++ COVID19 portfolio project.sql
@@ -1,7 +1,26 @@
+/*
+Mohammed Ahmed
+
+COVID-19 Data Exploration
+
+Skills Used: JOINS, CTE, TEMP TABLES, WINDOWS FUNCTIONS, AGGREGATE FUNCTIONS, CONVERTING DATA TYPES
+
+Data: The dataset is from https://ourworldindata.org/covid-deaths. It has data from January 2020 to November 2022. 
+I manipulated the data in Excel to contain the columns I wanted to work with.
+
+Task: The goal of this project is to explore 2020-2022 COVID-19 Data by asking and answering data exploration questions. 
+I choose to look at insights for the Australia since I am a Australian citizen. I also explored data globally and regionally.
+
+*/
+
+/* VIEWING THE DATASETS */
+
+-- Covid Deaths Data
+
 Select *
 From [Portfolio Project]..CovidDeaths$
 Where continent is not null 
-order by 3,4
+order by date,population
 
 
 -- Select Data that we are going to be starting with
@@ -9,28 +28,47 @@ order by 3,4
 Select Location, date, total_cases, new_cases, total_deaths, population
 From [Portfolio Project]..CovidDeaths$
 Where continent is not null 
-order by 1,2
+order by date,population
 
 
--- Total Cases vs Total Deaths
--- Shows likelihood of dying if you contract covid in your country
+-- How many COVID-19 cases in the Australia? How many deaths? What is the percentage death rate?
+-- This shows likelihood of dying if you contract covid in Australia 
 
-Select Location, date, total_cases,total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
+Select Location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as Death_Percentage
 From [Portfolio Project]..CovidDeaths$
 Where location like '%australia%'
-and continent is not null 
-order by 1,2
+order by date
 
+-- As of October 20 2022, there are 98,503,462 total cases and the death percentage is about 1.095%.
+-- This means that 98,503,462 people have been infected by COVID-19 since Jan 2020 and that there is about a 1.1% chance of dying if you contract COVID-19 while living in the US.
 
--- Total Cases vs Population
--- Shows what percentage of population infected with Covid
+-- How does this compare to a year ago, two years ago?
+
+Select Location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as Death_Percentage
+From [Portfolio Project]..CovidDeaths$
+Where location like '%australia%' AND date = '2021-10-22 00:00:00.000'
+order by date
+
+Select Location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as Death_Percentage
+From [Portfolio Project]..CovidDeaths$
+Where location like '%australia%' AND date = '2020-10-22 00:00:00.000'
+order by date
+
+-- One year ago, 48,024,223 people were infected with COVID-19 and there was a ~1.6% chance of dying upon contraction.
+-- Two years ago, 12,515,951 people were infected with COVID-19 and there was a ~2% chance of dying upon contraction.
+
+-- What percentage of the Australia population contracted COVID-19? */
+-- This Shows what percentage of population infected with Covid
 
 Select Location, date, Population, total_cases,  (total_cases/population)*100 as PercentPopulationInfected
 From [Portfolio Project]..CovidDeaths$
-order by 1,2
+Where location like '%australia%'
+order by date 
 
+-- 0.32% of the Australia population contracted COVID-19 as of October 20, 2022
 
--- Countries with Highest Infection Rate compared to Population
+-- Which countries have the highest infections rate?
+-- This shows countries with Highest Infection Rate compared to Population
 
 Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
 From [Portfolio Project]..CovidDeaths$
